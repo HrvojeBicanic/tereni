120083e044b64a7762a3ad7afe158a24e99c0e0c
Hrvoje Bičanić <hrvoje.bicanic3@gmail.com>
2024-05-24 20:00:02 +0200

    table tereni

--- tereni.sql
+++ tereni.sql
@@ -1,7 +1,13 @@
-create table termini (
+--create table termini (
+--id int,
+--user_id int,
+--pocetak varchar(45),
+--kraj varchar(45),
+--teren_id int
+--);
+
+create table tereni (
 id int,
-user_id int,
-pocetak varchar(45),
-kraj varchar(45),
-teren_id int
+naziv_terena varchar(45),
+tip_terena varchar(45)
 );
