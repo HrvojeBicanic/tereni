2bc560585c7b169ac174a93c4d4d4b0084ce547b
Hrvoje Bičanić <hrvoje.bicanic3@gmail.com>
2024-05-25 08:18:12 +0200

    dodan table card

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,7 +1,16 @@
-create table termin (
+--create table termin (
+--id int,
+--user_id int,
+--pocetak varchar(45),
+--kraj varchar(45),
+--teren_id int
+--);
+
+create table card (
 id int,
 user_id int,
-pocetak varchar(45),
-kraj varchar(45),
-teren_id int
+iznos int,
+broj_kartice int,
+cvv int,
+istek_kartice varchar(45)
 );
