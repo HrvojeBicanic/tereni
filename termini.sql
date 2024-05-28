46c666cee57b2b44aa17e7fec771b0988dda77ca
Hrvoje Bičanić <hrvoje.bicanic3@gmail.com>
2024-05-28 08:37:43 +0200

    dodan table teren

--- termini.sql
+++ termini.sql
@@ -19,11 +19,16 @@
 --iznos int
 --);
 
-create table termini (
-id int,
-user_id int,
-pocetak varchar(45),
-kraj varchar(45),
-tereni_id int,
-cijena decimal
+--create table termini (
+--id int,
+--user_id int,
+--pocetak varchar(45),
+--kraj varchar(45),
+--tereni_id int,
+--cijena decimal
+--);
+
+create table teren (
+naziv_terena varchar(45),
+tip_terena varchar(45)
 );
