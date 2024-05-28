2339e372662492d61f86948c8775bb9935b6cef3
Hrvoje Bičanić <hrvoje.bicanic3@gmail.com>
2024-05-28 08:21:45 +0200

    brisem staru talicu i mjenjam ju novom

---
+++ termini.sql
@@ -0,0 +1,13 @@
+--create database termini;
+
+--use termini;
+
+--create table "user" (
+--id int,
+--ime varchar(45),
+--broj_mob varchar(45),
+--email varchar(45),
+--prezime varchar(45),
+--password varchar(45)
+--);
+
